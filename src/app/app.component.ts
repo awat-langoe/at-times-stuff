26b0ae9393b453f28576f1fdabfc3d2666b26096
Awat Abdollahzadeh Langø <awat.langoe@gmail.com>
2024-08-18 16:07:47 +0200

    make header title dynamic based on route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { AsyncPipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import {
+  ActivatedRoute,
+  Event,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from "@angular/router";
+import { filter, map } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AsyncPipe, RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  protected readonly headerTitle$ = this.router.events.pipe(
+    filter(
+      (event: Event): event is NavigationEnd => event instanceof NavigationEnd,
+    ),
+    map(() => {
+      let title: string | undefined;
+
+      let route = this.activatedRoute.root;
+      while (route.firstChild) {
+        route = route.firstChild;
+        title = route.snapshot.title ?? title;
+      }
+
+      return title;
+    }),
+  );
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from "@angular/router";
 export const routes: Routes = [
   {
     path: "",
+    title: "At Times …",
     loadComponent: () =>
       import("./page/home/home.component").then((c) => c.HomeComponent),
     pathMatch: "full",
   },
   {
     path: "sound",
+    title: "At Times Sound",
     loadComponent: () =>
       import("./page/sound/sound.component").then((c) => c.SoundComponent),
   },
